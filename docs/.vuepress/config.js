0a629a3da30188b97f33d0cf1a05b4436a999e34
jayconscious <zhuzhiyong_wade@163.com>
2021-09-17 00:06:32 +0800

    chore: update version

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -81,9 +81,9 @@ module.exports = {
 				}
 			}
 			// Todo: 判断环境
-			// if (config.output && config.output.publicPath) {
-			// 	config.output.publicPath = 'https://unpkg.com/jayconscious-blog@latest/docs/.vuepress/dist/'
-			// }
+			if (config.output && config.output.publicPath) {
+				config.output.publicPath = 'https://unpkg.com/jayconscious-blog@latest/docs/.vuepress/dist/'
+			}
 			console.log('config', config)
     }
   }
