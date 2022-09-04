b107218ddac3653678000a22c1530dce375c5a40
zzy <zhuzhiyong@shizhuang-inc.com>
2022-09-04 13:41:45 +0800

    feat: 完成babel

--- demos/babelDemos/src/test.js
+++ demos/babelDemos/src/test.js
@@ -1,5 +1,33 @@
-const babel = require("@babel/core");
+// const babel = require("@babel/core");
+// const res = babel.transformSync("[1, 2, 3].map(n => n + 1)", {});
+// console.log(res)
 
-const res = babel.transformSync("[1, 2, 3].map(n => n + 1)", {});
+const fs = require('fs')
 
-console.log(res)
+const code = "function square(n) {return n * n;}"
+
+// const code = `function square(n) {
+//   return n * n;
+// }`;
+
+// const ast = parser.parse(code);
+
+const babelParser = require("@babel/parser")
+const traverse = require("@babel/traverse")
+
+const ast =  babelParser.parse(code)
+// const res = fs.writeFileSync('astOutput.json', JSON.stringify(ast, null, 4))
+
+console.log(typeof traverse)
+
+const traverseAst = traverse(ast, {
+  enter(path) {
+    if (path.isIdentifier({ name: "n" })) {
+      path.node.name = "x";
+    }
+  },
+})
+
+const res = fs.writeFileSync('traverseAstOutput.json', JSON.stringify(traverseAst, null, 4))
+
+
--- docs/engineering/babel/basicUse.md
+++ docs/engineering/babel/basicUse.md
@@ -16,3 +16,5 @@ sticky: 1
 ## Babel 简介
 那么什么是`Babel`呢？在`Babel`的官网上有明确的回答。`Babel`是一个 `JavaScript compiler`。Babel 是一个工具链，主要用于在当前和旧的浏览器或环境中，将 ECMAScript 2015+ 代码转换为 JavaScript 向后兼容版本的代码。
 
+
+
